d6c96d225b01abf5ece60b07f9b88eab649f216c
Dallas-Alfonso <51836155+Dallas-Alfonso@users.noreply.github.com>
2020-11-08 13:04:22 -0500

    end of part 19. need to configure why description isnt showing

--- backend/data.js
+++ backend/data.js
@@ -16,7 +16,6 @@ const data = {
     ],
     products:[
         {
-            _id:'1',
             name:'Nike Slim Shirt',
             category: 'Shirts',
             image: '/images/p1.jpg',
@@ -25,10 +24,9 @@ const data = {
             brand: 'Nike',
             rating: 4.5, 
             numReviews: 10,
-            descritpion: 'high quality product',
+            description: 'high quality product',
         },
         {
-            _id:'2',
             name:'Adidas Fit Shirt',
             category: 'Shirts',
             image: '/images/p2.jpg',
@@ -37,10 +35,9 @@ const data = {
             brand: 'Adidas',
             rating: 4.0,
             numReviews: 10,
-            descritpion: 'high quality product',
+            description: 'high quality product',
         },
         {
-            _id:'3',
             name:'Lacoste Free Shirt',
             category: 'Shirts',
             image: '/images/p3.jpg',
@@ -49,10 +46,9 @@ const data = {
             brand: 'Lacoste',
             rating: 4.8,
             numReviews: 17,
-            descritpion: 'high quality product',
+            description: 'high quality product',
         },
         {
-            _id:'4',
             name:'Nike Slim Pant',
             category: 'Pants',
             image: '/images/p4.jpg',
@@ -61,10 +57,9 @@ const data = {
             brand: 'Nike',
             rating: 4.5,
             numReviews: 14,
-            descritpion: 'high quality product',
+            description: 'high quality product',
         },
         {
-            _id:'5',
             name:'Puma Slim Pant',
             category: 'Pants',
             image: '/images/p5.jpg',
@@ -73,10 +68,9 @@ const data = {
             brand: 'Puma',
             rating: 4.5,
             numReviews: 10,
-            descritpion: 'high quality product',
+            description: 'high quality product',
         },
         {
-            _id:'6',
             name:'Adidas Fit Pant',
             category: 'Pants',
             image: '/images/p6.jpg',
@@ -85,7 +79,7 @@ const data = {
             brand: 'Adidas',
             rating: 4.5,
             numReviews: 15,
-            descritpion: 'high quality product',
+            description: 'high quality product',
         }
     ]
 }
