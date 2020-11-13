a4a1bccd160a9094b5f10e4d528d396c2060e463
Dallas-Alfonso <51836155+Dallas-Alfonso@users.noreply.github.com>
2020-11-12 20:52:14 -0500

    no errors thus far. end of part 24.  Shipping adressScree.js might be confusing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,18 +6,19 @@ import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SigninScreen from './screens/SigninScreen';
 
 
 function App() {
-  const cart = useSelector(state => state.cart);
+  const cart = useSelector((state) => state.cart);
   const {cartItems} = cart;
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   const dispatch = useDispatch();
   const signoutHandler = () =>{
     dispatch(signout());
-  }
+  };
   return (
     <BrowserRouter>
     <div className="grid-container">
@@ -38,9 +39,11 @@ function App() {
                     <Link to="#">{userInfo.name} <i className="fa fa-caret-down"></i>{' '} 
                     </Link>
                     <ul className="dropdown-content">
+                      <li>
                       <Link to="#signout" onClick={signoutHandler}>
                         Sign Out
                       </Link>
+                      </li>
                     </ul>
                     </div>
                     ) : (
@@ -55,6 +58,7 @@ function App() {
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/signin" component={SigninScreen}></Route>
           <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingAddressScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
