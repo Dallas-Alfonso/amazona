daaad6a341d75379f21325d5e10ec46c17ee41ed
Dallas-Alfonso <51836155+Dallas-Alfonso@users.noreply.github.com>
2021-01-10 15:37:00 -0500

    no errors. video 58

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcryptjs';
+
 const data = {
     users: [
         {
@@ -6,6 +7,14 @@ const data = {
             email: 'admin@example.com',
             password: bcrypt.hashSync('1234', 8),
             isAdmin: true,
+            isSeller: true,
+            seller: {
+                name: 'Puma',
+                logo: '/images/logo1.png',
+                description: 'best seller',
+                rating: 4.5,
+                numReviews: 120,
+            },
         },
         {
             name: 'John',
