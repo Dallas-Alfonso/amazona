db4a4fee4e3d30f0322e9e834b43563d5887e650
Dallas-Alfonso <51836155+Dallas-Alfonso@users.noreply.github.com>
2021-01-15 13:28:54 -0500

    minor fixes

--- backend/server.js
+++ backend/server.js
@@ -35,11 +35,15 @@ app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
 
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
+
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message});
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server at http://localhost:${port}`);
+    console.log(`Serve at http://localhost:${port}`);
 });
